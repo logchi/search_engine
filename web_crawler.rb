4423e982e128672c94b4df4e9229dcb973e6e3da
SuKong <logchi@gmail.com>
2013-10-29 17:19:32 +0800

    A simple-stupid crawler

--- web_crawler.rb
+++ web_crawler.rb
@@ -1,20 +1,40 @@
 require 'net/http'
 
-page = Net::HTTP.get('stackoverflow.com', '/index.html')
+def web_crawl(seed)
+  tocrawl = [seed]
+  crawled = []
+  until tocrawl.empty?
+    seed_link = tocrawl.pop
+    unless crawled.include?(seed_link)
+      crawled << seed_link
+      tocrawl = tocrawl | find_all_links( get_page(seed_link) )
+    end
+  end
+  crawled
+end
+
+def get_page(link)
+  begin
+    Net::HTTP.get( URI(link) )
+  rescue Exception
+    ""
+  end
+end
 
 def find_link(page, start_anchor)
-  start_href   = page.index('href=', start_anchor)
-  start_link   = page.index('"', start_href) + 1
-  end_link     = page.index('"', start_link) - 1
-  link         = page[start_link..end_link]
+  start_href = page.index('href=', start_anchor)
+  start_link = page.index('"', start_href) + 1
+  end_link   = page.index('"', start_link) - 1
+  link       = page[start_link..end_link]
   return link, end_link + 1
 end
 
-links = []
-start = 0
-while start_anchor = page.index(/<\s*a.+href=/, start) 
-  link, start = find_link(page, start_anchor)
-  links << link
+def find_all_links(page)
+  links = []
+  start = 0
+  while start_anchor = page.index(/<\s*a\s.*href=/, start) 
+    link, start = find_link(page, start_anchor)
+    links << link
+  end
+  links
 end
-
-puts links
