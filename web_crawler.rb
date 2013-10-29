b26187109732eb4b090125c582f3eebd9e04af79
SuKong <logchi@gmail.com>
2013-10-29 11:15:15 +0800

    Find all links of a web page.

--- web_crawler.rb
+++ web_crawler.rb
@@ -1,8 +1,20 @@
-page = '<a class="dropdown-toggle" href="/courses">Course Catalog</a>'
-start_anchor = page.index(/<\s*a.+href=/)
-start_href = page.index('href=', start_anchor)
-start_link = page.index('"', start_href) + 1
-end_link = page.index('"', start_link) - 1
-first_link = page[start_link..end_link]
-puts first_link
+require 'net/http'
 
+page = Net::HTTP.get('stackoverflow.com', '/index.html')
+
+def find_link(page, start_anchor)
+  start_href   = page.index('href=', start_anchor)
+  start_link   = page.index('"', start_href) + 1
+  end_link     = page.index('"', start_link) - 1
+  link         = page[start_link..end_link]
+  return link, end_link + 1
+end
+
+links = []
+start = 0
+while start_anchor = page.index(/<\s*a.+href=/, start) 
+  link, start = find_link(page, start_anchor)
+  links << link
+end
+
+puts links
